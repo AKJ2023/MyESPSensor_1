7aaa771656f7689820655cb294bbf50fce4b2362
AKJ2023 <121627585+AKJ2023@users.noreply.github.com>
2025-01-20 10:40:50 +0100

    asd

--- components/sensor1/__init__.py
+++ components/sensor1/__init__.py
@@ -1,1 +1,27 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor
+from esphome.const import CONF_ID, UNIT_CELSIUS, ICON_THERMOMETER, DEVICE_CLASS_TEMPERATURE
 
+DEPENDENCIES = []
+
+# Define a new component namespace
+my_sensor_ns = cg.esphome_ns.namespace("my_sensor")
+MySensor = my_sensor_ns.class_("MySensor", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(MySensor),
+    cv.Optional("temperature"): sensor.sensor_schema(
+        UNIT_CELSIUS,
+        ICON_THERMOMETER,
+        1,  # Accuracy decimals
+        DEVICE_CLASS_TEMPERATURE,
+    ),
+})
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    if "temperature" in config:
+        sens = yield sensor.new_sensor(config["temperature"])
+        cg.add(var.set_temperature_sensor(sens))
+    yield cg.register_component(var, config)
