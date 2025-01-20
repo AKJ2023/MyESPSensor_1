27d9b80c466fc728cd4fbf18bf87cfd6ec004e33
AKJ2023 <121627585+AKJ2023@users.noreply.github.com>
2025-01-20 10:48:39 +0100

    asd

--- components/sensor1/__init__.py
+++ components/sensor1/__init__.py
@@ -3,14 +3,13 @@ import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID, UNIT_CELSIUS, ICON_THERMOMETER, DEVICE_CLASS_TEMPERATURE
 
-DEPENDENCIES = []
+# Define namespace
+sensor1_ns = cg.esphome_ns.namespace("sensor1")
+Sensor1Component = sensor1_ns.class_("Sensor1Component", cg.PollingComponent)
 
-# Define a new component namespace
-my_sensor_ns = cg.esphome_ns.namespace("my_sensor")
-MySensor = my_sensor_ns.class_("MySensor", cg.Component)
-
+# Schema for the component
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(MySensor),
+    cv.GenerateID(): cv.declare_id(Sensor1Component),
     cv.Optional("temperature"): sensor.sensor_schema(
         UNIT_CELSIUS,
         ICON_THERMOMETER,
@@ -20,8 +19,9 @@ CONFIG_SCHEMA = cv.Schema({
 })
 
 def to_code(config):
+    # Create a new instance of Sensor1Component
     var = cg.new_Pvariable(config[CONF_ID])
     if "temperature" in config:
         sens = yield sensor.new_sensor(config["temperature"])
-        cg.add(var.set_temperature_sensor(sens))
+        cg.add(var.set_temperature_sensor(sens))  # Add temperature sensor to the component
     yield cg.register_component(var, config)
