6b2b832e380bf72a3e975543aaf59b709e249617
AKJ2023 <121627585+AKJ2023@users.noreply.github.com>
2025-01-20 10:25:36 +0100

    update

--- components/sensor1/sensor1.h
+++ components/sensor1/sensor1.h
@@ -3,6 +3,30 @@
 #include "esphome.h"
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
+#include "dht11/dht11.h"
+
+
+static const uint8_t D0   = 3;
+static const uint8_t D1   = 1;
+static const uint8_t D2   = 16;
+static const uint8_t D3   = 5;
+static const uint8_t D4   = 4;
+static const uint8_t D5   = 14;
+static const uint8_t D6   = 12;
+static const uint8_t D7   = 13;
+static const uint8_t D8   = 0;
+static const uint8_t D9   = 2;
+static const uint8_t D10  = 15;
+static const uint8_t D11  = 13;
+static const uint8_t D12  = 12;
+static const uint8_t D13  = 14;
+static const uint8_t D14  = 4;
+static const uint8_t D15  = 5;
+
+#define DHT11PIN D0
+
+dht11 my_dht_sensor;
+
 
 namespace esphome
 {
@@ -23,11 +47,11 @@ namespace esphome
             }
             
             void update() override{
-                ESP_LOGV(TAG, "Updating");
-            }
+                ESP_LOGV(TAG, "Updating");    
+                
+                int chk = my_dht_sensor.read(DHT11PIN);
+                publish_state(my_dht_sensor.temperature);
 
-            void loop() override{
-                ESP_LOGV(TAG, "Looping");
             }
 
         protected:
